065817a84ec707ddbf473576f6bff9e919c58d3c
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-11-14 09:55:35 -0300

    class
    melhora da class

--- paciente.py
+++ paciente.py
@@ -1,8 +1,17 @@
-class Paciente:
 
+
+class Paciente:
     def __init__(self, nome, idade, cpf, email):
         print('Acessei o construtor')
         self.nome = nome
         self.idade = idade
         self.cpf = cpf
         self.email = email
+
+
+from paciente import Paciente
+
+
+paciente = Paciente('paulo', 33, '001.002.003.45', 'paulo.roberto@gmail')
+print(paciente.nome)
+print(paciente.idade)
