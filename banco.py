f5fe89cf42562f4e41fac92b1b77ef539a9ba7f5
Paulo Roberto <paulo.trabalho10@gmail.com>
2022-11-17 10:57:58 -0300

    Update banco.py
    agora o banco pode  pode fazer transferência entre os clientes

--- banco.py
+++ banco.py
@@ -1,19 +1,37 @@
 class Conta:
-    
-    def __init__(self, numero, titular, saldo):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
 
+    taxa = 0.50
+
+    @staticmethod
+    def retornaCodigoBanco():
+        return '345'
+
+# Atributos de Istacias
+    def __init__(self, numero, titular, saldo=2000):
+        self._numero = numero     # protegida (protected)
+        self.titular = titular    # publica (public)
+        self.__saldo = saldo      # privada (private)
 
     def extrato(self):
-        print(f'Saldo : R$ {self.saldo}')
-        
+        print(f'Saldo : R$ {self.__saldo}')
+
     def deposito(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
         print(f'{self.titular} deposito realizado')
-    
+
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
         print(f'{self.titular} saque realizado')
-        
+
+    def transferir(self, valor, origem, destino):
+        origem.sacar(valor)
+        destino.deposito(valor)
+
+
+conta1 = Conta(123, 'Paulo Roberto', 3000)
+conta2 = Conta(124, 'Pedro Souza', 3000)
+print(conta1.__dict__)
+print(conta2.__dict__)
+conta1.transferir(300, conta1, conta2)
+conta1.extrato()
+conta2.extrato()
